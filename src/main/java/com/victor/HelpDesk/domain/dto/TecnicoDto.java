1392949fe1d15fe6dac3dd708e725e38eb1c0594
unknown <alvesmlktop@gmail.com>
2023-12-04 13:07:25 -0300

    padrao DTO implementado

---
+++ src/main/java/com/victor/HelpDesk/domain/dto/TecnicoDto.java
@@ -0,0 +1,97 @@
+package com.victor.HelpDesk.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.victor.HelpDesk.domain.Tecnico;
+import com.victor.HelpDesk.domain.enums.Perfil;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TecnicoDto implements Serializable {
+    private static final long serialVersionUid = 1L;
+
+
+    protected Integer id;
+    protected String nome;
+    protected String cpf;
+    protected String email;
+    protected String senha;
+    protected Set<Integer> perfils = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public TecnicoDto(){
+        super();
+    }
+
+    public TecnicoDto(Tecnico obj) {
+       super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfils = obj.getPerfils().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfils() {
+        return perfils.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfils.add(perfil.getCodigo());
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
--- src/main/java/com/victor/HelpDesk/resources/TecnicoResource.java
+++ src/main/java/com/victor/HelpDesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.victor.HelpDesk.resources;
 
 import com.victor.HelpDesk.domain.Tecnico;
+import com.victor.HelpDesk.domain.dto.TecnicoDto;
 import com.victor.HelpDesk.serveces.TecnicoService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,9 @@ public class TecnicoResource {
     @Autowired
     private TecnicoService service;
     @GetMapping(value ="/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+    public ResponseEntity<TecnicoDto> findById(@PathVariable Integer id){
     Tecnico obj = service.findById(id);
-    return ResponseEntity.ok().body(obj);
+    return ResponseEntity.ok().body(new TecnicoDto(obj));
 
 }
 }
